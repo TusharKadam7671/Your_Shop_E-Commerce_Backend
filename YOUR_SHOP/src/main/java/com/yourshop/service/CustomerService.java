ee96b292f514e2f310531c596ad553f455c45a20
TusharKadam7671 <kadamtushar080@gmail.com>
2022-12-12 23:16:15 +0530

    customer module done

--- YOUR_SHOP/src/main/java/com/yourshop/service/CustomerService.java
+++ YOUR_SHOP/src/main/java/com/yourshop/service/CustomerService.java
@@ -1,10 +1,16 @@
 package com.yourshop.service;
 
+import java.util.List;
+
 import com.yourshop.exception.CustomerException;
 import com.yourshop.model.Customer;
 
 public interface CustomerService {
 	
-	public Customer createCustomer(Customer customer) throws CustomerException;
+	public Customer addCustomer(Customer cust) throws CustomerException;
+	public Customer updateCustomer(Customer cust) throws CustomerException;
+	public Customer removeCustomer(Integer customerId) throws CustomerException;
+	public Customer viewCustomer(Integer CustomerId) throws CustomerException;
+	public List<Customer> viewAllCustomers(String location) throws CustomerException;
 
 }
