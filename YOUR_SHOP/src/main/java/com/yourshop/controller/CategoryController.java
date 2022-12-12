df6ce3f47069b227f641aec17dac4e2b63ef9da9
TusharKadam7671 <kadamtushar080@gmail.com>
2022-12-12 23:15:56 +0530

    category module - some part commented

--- YOUR_SHOP/src/main/java/com/yourshop/controller/CategoryController.java
+++ YOUR_SHOP/src/main/java/com/yourshop/controller/CategoryController.java
@@ -1,70 +1,71 @@
-package com.yourshop.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.yourshop.exception.CategoryException;
-import com.yourshop.model.Category;
-import com.yourshop.model.Product;
-import com.yourshop.service.CategoryService;
-
-@RestController
-@RequestMapping("/category")
-public class CategoryController {
-	
-	@Autowired
-	private CategoryService catService;
-	
-	@PostMapping("/add")
-	public ResponseEntity<Category> addCategory(@RequestBody Category category) throws CategoryException 
-	{
-		Category savedCategory =catService.createCategory(category);
-		
-		return new ResponseEntity<Category>(savedCategory,HttpStatus.CREATED);
-	}
-	
-	@GetMapping("/view/{id}")
-	public ResponseEntity<Category> viewCategory(@PathVariable("id") int categoryId) throws CategoryException 
-	{
-		Category category = catService.viewCategory(categoryId);
-		
-		return new ResponseEntity<Category>(category,HttpStatus.OK);
-		
-	}
-	
-	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<Category> deleteCategory(@PathVariable("id") int categoryId) throws CategoryException 
-	{
-		Category category = catService.removeCategory(categoryId);
-		
-		return new ResponseEntity<Category>(category,HttpStatus.ACCEPTED);
-	}
-	
-	@GetMapping("/viewAll")
-	public ResponseEntity<List<Category>> allCategory() throws CategoryException 
-	{
-		List<Category> categories = catService.allCategories();
-		
-		return new ResponseEntity<List<Category>>(categories,HttpStatus.OK);
-	}
-	
-	@GetMapping("/viewProductByCategory/{id}")
-	public ResponseEntity<List<Product>> productByCategory(@PathVariable("id") int categoryId) throws CategoryException 
-	{
-		List<Product> products = catService.productsByCategory(categoryId);
-		
-		return new ResponseEntity<List<Product>>(products,HttpStatus.OK);
-	}
-
-
-}
+//package com.yourshop.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.yourshop.exception.CategoryException;
+//import com.yourshop.model.Category;
+//import com.yourshop.model.Product;
+//import com.yourshop.service.CategoryService;
+//
+//@RestController
+//@RequestMapping("/category")
+//public class CategoryController {
+//	
+//	@Autowired
+//	private CategoryService catService;
+//	
+//	@PostMapping("/add")
+//	public ResponseEntity<Category> addCategory(@RequestBody Category category) throws CategoryException 
+//	{
+//		Category savedCategory =catService.createCategory(category);
+//		
+//		return new ResponseEntity<Category>(savedCategory,HttpStatus.CREATED);
+//	}
+//	
+//	@GetMapping("/view/{id}")
+//	public ResponseEntity<Category> viewCategory(@PathVariable("id") int categoryId) throws CategoryException 
+//	{
+//		Category category = catService.viewCategory(categoryId);
+//		
+//		return new ResponseEntity<Category>(category,HttpStatus.OK);
+//		
+//	}
+//	
+//	@DeleteMapping("/delete/{id}")
+//	public ResponseEntity<Category> deleteCategory(@PathVariable("id") int categoryId) throws CategoryException 
+//	{
+//		Category category = catService.removeCategory(categoryId);
+//		
+//		return new ResponseEntity<Category>(category,HttpStatus.ACCEPTED);
+//	}
+//	
+//	@GetMapping("/viewAll")
+//	public ResponseEntity<List<Category>> allCategory() throws CategoryException 
+//	{
+//		List<Category> categories = catService.allCategories();
+//		
+//		return new ResponseEntity<List<Category>>(categories,HttpStatus.OK);
+//	}
+//	
+//	/*
+//	@GetMapping("/viewProductByCategory/{id}")
+//	public ResponseEntity<List<Product>> productByCategory(@PathVariable("id") int categoryId) throws CategoryException 
+//	{
+//		List<Product> products = catService.productsByCategory(categoryId);
+//		
+//		return new ResponseEntity<List<Product>>(products,HttpStatus.OK);
+//	}
+//*/
+//
+//}
--- YOUR_SHOP/src/main/java/com/yourshop/model/Category.java
+++ YOUR_SHOP/src/main/java/com/yourshop/model/Category.java
@@ -2,12 +2,16 @@ package com.yourshop.model;
 
 import java.util.List;
 
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -16,23 +20,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class Category {
+
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer catId;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer categoryId;
-	
+	@NotNull(message = "Category name can not be null")
+	@NotBlank(message = "Category name can not be blank")
+	@NotEmpty(message = "Category name can not be empty")
 	private String categoryName;
 	
-
-//	@OneToMany
-	@JsonIgnore
-	@OneToMany(mappedBy="category", cascade = CascadeType.ALL)
-	private List<Product> product;
-
 }
